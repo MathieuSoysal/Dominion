cc6127fcfa2d17d9879ef89c2915abd536aec23f
HouillonAnd <andy.houillon@etu.umontpellier.fr>
2019-05-09 08:02:04 +0200

    Remodel => GOOD

--- src/main/java/fr/umontpellier/iut/dominion/cards/base/Remodel.java
+++ src/main/java/fr/umontpellier/iut/dominion/cards/base/Remodel.java
@@ -1,6 +1,7 @@
 package fr.umontpellier.iut.dominion.cards.base;
 
 import fr.umontpellier.iut.dominion.CardType;
+import fr.umontpellier.iut.dominion.Player;
 import fr.umontpellier.iut.dominion.cards.Card;
 
 import java.util.ArrayList;
@@ -15,6 +16,13 @@ import java.util.List;
 public class Remodel extends Card {
     public Remodel() {
         super("Remodel", 4);
+    }
+
+    @Override
+    public void play(Player p) {
+        Card removed = p.removeFromHand(p.chooseCard("Choisissez une carte à placer sur le dessus de votre pioche :", p.getCardsInHand(), false));
+        int costToAdd = removed.getCost();
+        p.gainFromSupply(p.chooseCard("Choisissez une carte coutant jusqu'à 4 :",p.listCardCostingUpTo(costToAdd + 2),false));
     }
 
     @Override
